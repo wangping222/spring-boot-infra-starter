e65bca5db6ab4a8a8245b1e4134538bca81dc066
wangping <wangping222@qbitnetwork.com>
2025-11-19 17:58:47 +0800

    refactor(ExcelCellDescriptor): 使用Lombok简化代码
    移除手动编写的构造函数和getter方法，改用Lombok的@AllArgsConstructor和@Getter注解

--- excel-starter/src/main/java/com/qbit/framework/business/excel/starter/ExcelCellDescriptor.java
+++ excel-starter/src/main/java/com/qbit/framework/business/excel/starter/ExcelCellDescriptor.java
@@ -1,18 +1,17 @@
 package com.qbit.framework.business.excel.starter;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public class ExcelCellDescriptor<T> {
     private final String header;
     private final java.util.function.Function<T, Object> valueExtractor;
 
-    public ExcelCellDescriptor(String header, java.util.function.Function<T, Object> valueExtractor) {
-        this.header = header;
-        this.valueExtractor = valueExtractor;
-    }
 
     public static <T> ExcelCellDescriptor<T> of(String header, java.util.function.Function<T, Object> valueExtractor) {
         return new ExcelCellDescriptor<>(header, valueExtractor);
     }
 
-    public String getHeader() { return header; }
-    public java.util.function.Function<T, Object> getValueExtractor() { return valueExtractor; }
 }
