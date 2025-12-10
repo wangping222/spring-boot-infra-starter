447c487a46b319dfaa3e17e84f3579459e957618
wangping <wangping222@qbitnetwork.com>
2025-12-10 11:25:58 +0800

    重命名

--- tests/src/test/java/com/qbit/framework/api/FeignApiPropertiesBindingTests.java
+++ tests/src/test/java/com/qbit/framework/api/FeignApiPropertiesBindingTests.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,6 +28,18 @@ class FeignApiPropertiesBindingTests {
     void bindsFromApplicationYaml() {
         assertNotNull(props.getBaseUrl());
         assertNotNull(props.getSecret());
+    }
+
+
+
+    @Test
+    void testExpression(){
+        StandardEvaluationContext context = new StandardEvaluationContext();
+
+        SpelExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("3*5");
+        Object value = expression.getValue(context);
+        System.out.println(value);
     }
 }
 
